1e3f5c71dc5f8c2973a1a04def4bd41bf49c7580
Marc Schwiesow <aeonictrust@gmail.com>
2020-02-25 14:57:16 -0600

    Component framework setup.  Added Shopping List, Recipes, Header components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,25 @@
+import { ShoppingListComponent } from './features/shopping-list/shopping-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecipesComponent } from './features/recipes/recipes.component';
+import { RecipeListComponent } from './features/recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './features/recipes/recipe-detail/recipe-detail.component';
+import { ShoppingListEditComponent } from './features/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeItemComponent } from './features/recipes/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    HeaderComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
