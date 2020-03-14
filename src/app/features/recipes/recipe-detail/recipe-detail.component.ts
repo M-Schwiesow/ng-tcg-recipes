30eaaf9a4343886a0b1701af9ab3edd65605e57c
Marc Schwiesow <aeonictrust@gmail.com>
2020-03-14 09:31:50 -0500

    Added ability to delete a recipe from RecipeDetail Component.

--- src/app/features/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/features/recipes/recipe-detail/recipe-detail.component.ts
@@ -2,7 +2,7 @@ import { RecipeService } from './../recipe.service';
 import { ShoppingListService } from './../../shopping-list/shopping-list.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -13,7 +13,7 @@ export class RecipeDetailComponent implements OnInit {
 
   recipe: Recipe;
 
-  constructor(private shoppingListService: ShoppingListService, private recipeService: RecipeService, private route: ActivatedRoute) { }
+  constructor(private shoppingListService: ShoppingListService, private recipeService: RecipeService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(
@@ -25,6 +25,11 @@ export class RecipeDetailComponent implements OnInit {
 
   sendToShoppingList() {
     this.shoppingListService.addMany(this.recipe.ingredients);
+  }
+
+  onDeleteRecipe() {
+    this.recipeService.deleteRecipe(this.recipe);
+    this.router.navigate(['../']);
   }
 
 }
