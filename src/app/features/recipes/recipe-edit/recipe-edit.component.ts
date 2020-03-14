72caaa55d724a5b2db8e2ae22e711381eba3273f
Marc Schwiesow <aeonictrust@gmail.com>
2020-03-14 09:33:18 -0500

    Filled out RecipeEdit Component to create new/update existing recipes.  Updating RecipeList Component on changes to recipes.

--- src/app/features/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/features/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,8 +1,8 @@
 import { Recipe } from './../recipe.model';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { RecipeService } from '../recipe.service';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -16,7 +16,7 @@ export class RecipeEditComponent implements OnInit {
   recipe: Recipe;
   recipeForm: FormGroup;
 
-  constructor(private recipeService: RecipeService, private route: ActivatedRoute) { }
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.route.paramMap
@@ -31,25 +31,87 @@ export class RecipeEditComponent implements OnInit {
   }
 
   private initForm() {
+    let recipeId = null;
     let recipeName = '';
     let recipeImagePath = '';
     let recipeDescription = '';
+    let recipeIngredients = new FormArray([]);
 
     if(this.editMode) {
+      recipeId = this.recipe.id;
       recipeName = this.recipe.name;
       recipeImagePath = this.recipe.imagePath;
       recipeDescription = this.recipe.description;
+      if(this.recipe['ingredients']) {
+        for (let ingredient of this.recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              'name': new FormControl(ingredient.name, Validators.required),
+              'amount': new FormControl(ingredient.amount, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
+            })
+          );
+        }
+      }
     }
     this.recipeForm = new FormGroup({
-      'name': new FormControl(recipeName),
-      'imagePath': new FormControl(recipeImagePath),
-      'description': new FormControl(recipeDescription)
+      // this looks unnecessarily difficult, however we're still generating our own ids
+      'id': new FormControl(this.editMode ? recipeId : Math.floor(Math.random()*1000)),
+      'name': new FormControl(recipeName, Validators.required),
+      'imagePath': new FormControl(recipeImagePath, Validators.required),
+      'description': new FormControl(recipeDescription, Validators.required),
+      'ingredients': recipeIngredients
     });
   }
 
-  onSubmit(){
-    console.log("do a jig");
-    console.log(this.recipeForm);
+  onSubmit() {
+    let formValues = this.recipeForm.value;
+    if(this.recipeForm.valid) {
+      if(this.editMode) {
+        this.recipe.name = formValues['name'];
+        this.recipe.description = formValues['description'];
+        this.recipe.imagePath = formValues['imagePath'];
+        this.recipe.ingredients = formValues['ingredients'];
+        this.recipeService.updateRecipe(this.recipe);
+      } else {
+        /**
+         * If our form's value property matches the format of our data model,
+         * we can simply assign the value of the form directly to a new object
+         * which is typed to our data model, like so:
+         * let newRecipe: Recipe = this.recipeForm.value;
+         * 
+         * Does our ID field throw a wrench in that capability?  Yes, yes it does.
+         */
+        let newRecipe: Recipe = new Recipe(
+          formValues['name'], 
+          formValues['description'], 
+          formValues['imagePath'], 
+          formValues['ingredients']);
+        this.recipeService.addRecipe(newRecipe);
+      }
+    }
+    this.router.navigate(['../']);
+
+  }
+
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(null, Validators.required),
+        'amount': new FormControl(null, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
+      })
+    );
+  }
+
+  onCancel() {
+    this.router.navigate(['../']);
+  }
+
+  onDeleteIngredient(index: number) {
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
+
+  get controls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
   }
 
 }
--- src/app/features/recipes/recipe-list/recipe-list.component.ts
+++ src/app/features/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
+import { Subscription } from 'rxjs';
 import { RecipeService } from './../recipe.service';
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +8,22 @@ import { Recipe } from '../recipe.model';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
 
   recipes: Recipe[];
+  private recipeSubscription: Subscription;
 
   constructor(private recipeService: RecipeService) { }
   ngOnInit(): void {
+    // consider changing to a subscription, or see if routing from recipe-edit is effective
     this.recipes = this.recipeService.getRecipes();
+    this.recipeSubscription = this.recipeService.recipesChanged.subscribe(
+      (recipes: Recipe[]) => this.recipes = recipes
+    );
+  }
+
+  ngOnDestroy() {
+    this.recipeSubscription.unsubscribe();
   }
 
   onSelectRecipe(selectedRecipe: Recipe): void {
